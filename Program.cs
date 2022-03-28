913f3e4ec04a5554f52a6f4fa689a9848c0e8ac5
Polat <emre.polat@siemens.com>
2022-03-28 14:07:05 +0300

    hello

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Console_review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hangi sayiya kadar toplanacak? ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            for (int i = 0; i <= sayi; i++)
+            {
+                toplam += i;
+            }
+            Console.WriteLine("sonuc " + toplam.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
