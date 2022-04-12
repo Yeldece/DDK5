d1d9f5afe64500a55b99ec1e51f2602f4808cea8
Yeldece <43844741+Yeldece@users.noreply.github.com>
2022-04-12 11:56:19 +0300

    New Classes, new questions-answers

--- Program.cs
+++ Program.cs
@@ -1,19 +1,60 @@
 using System;
+using System.Threading.Tasks;
 namespace Console_review
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            MovieManager mvManager = new MovieManager();
-            // Console.WriteLine("Enter a start year for filtering Imdb top 250: ");
-            // int startingYear = Convert.ToInt32(Console.ReadLine());
-            // Console.WriteLine("Enter an ending year for filter: ");
-            // int endingYear = Convert.ToInt32(Console.ReadLine());
-            //            mvManager.PrintMovieList(mvManager.FilterBetweenYears(mvManager.GetTop(), startingYear, endingYear));
-            //mvManager.PrintMovieList(mvManager.GetTop());
-            Console.WriteLine(mvManager.GetNumBetweenRating(mvManager.GetTop(), 0, 10));
-            mvManager.PrintMovieList(mvManager.FilterBetweenRating(mvManager.GetTop(), 9.0, 9.5));
+            ProductManager pManager = new ProductManager();
+            await pManager.GetProducts();
+            List<Product> productList = pManager.ProductList;
+            //Fiyati 50'den kucuk kac adet urun var
+            //How many product price are less than 50
+            int countPriceLessFifty = productList.FindAll(x => x.unitPrice < 50).Count();
+            //70
+
+            // CategoryId si 3 ve SupplierId si 2 olan kaç adet ürün var?
+            //How many of their catagoryId is = 3 and suplier = 2
+            int answer2 = productList.FindAll(x => x.categoryId == 3 && x.supplierId == 2).Count();
+            //1
+            //Fiyatı 50-100 arasında kaç adet ürün var?
+            //Price between 50 and 100
+            int priceFiftyToHundred = productList.FindAll(x => x.unitPrice >= 50 && x.unitPrice < 100).Count();
+            //5
+            // İsmi a harfi ile başlayan ürünleri console a yazdır.
+            //print out the product names which start with 'a'
+            Product[] nameListStartsWithA = productList.FindAll(x => x.name.ToLower().StartsWith('a')).ToArray();
+            foreach (Product p in nameListStartsWithA)
+            {
+                Console.WriteLine(p.name);
+            }
+            //İsminde a harfi geçen ürünleri ekrana yazdır
+            //Product names which contain a
+            var namesContainA = productList.FindAll(x => x.name.ToLower().Contains('a'));
+            Console.WriteLine("Names which contain A");
+            foreach (var p in namesContainA)
+            {
+                Console.WriteLine(p.name);
+            }
+            // Stok adedi 10 un altında kalan kaç ürün var?
+            //How many of product's stock under 10
+            int stockUnderTen = productList.FindAll(x => x.unitsInStock < 10).Count();
+            //12
+            // Stok adedi 10 un altında olan ürünleri console a yazdır.
+            //Print the product names with stock under 10
+            var stockUnderTenList = productList.FindAll(x => x.unitsInStock < 10).ToArray();
+            Console.WriteLine("Warning! This products' stock under 10");
+            foreach (var p in stockUnderTenList)
+            {
+                Console.WriteLine(p.name);
+            }
+            //Tüm ürünlerin ortalama fiyatı nedir?
+            //What is the average price
+            decimal avgPrice = productList.Average(x => x.unitPrice);
+            // CategoryId si 3 olan ürünlerin ortalama fiyatı nedir?
+            //What is the average price for catagory 3
+            decimal avgPriceCatThree = productList.FindAll(x => x.categoryId == 3).Average(x => x.unitPrice);
         }
     }
 }
