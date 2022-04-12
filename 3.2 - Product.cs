f327781aa822794ca06caa95737f6e3d48d19a8e
Yeldece <43844741+Yeldece@users.noreply.github.com>
2022-04-12 11:56:45 +0300

    New Classes, new questions-answers

---
+++ 3.2 - Product.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Console_review
+{
+    class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal unitPrice { get; set; }
+        public int unitsInStock { get; set; }
+        public int categoryId { get; set; }
+        public int supplierId { get; set; }
+    }
+}
