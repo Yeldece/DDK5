0b9bd9aaf73a4e46eea14211eee34a63bcb5c293
Yeldece <43844741+Yeldece@users.noreply.github.com>
2022-06-11 11:38:03 +0300

    Added update functionality

--- FridayQuestions/7. 10 Jun - ASPNetCore and EFCore/Controllers/HomeController.cs
+++ FridayQuestions/7. 10 Jun - ASPNetCore and EFCore/Controllers/HomeController.cs
@@ -26,13 +26,38 @@ namespace MyWebApp.Controllers
         public IActionResult Add(Product newProduct)
         {
             var existingCategory = NorthwindContext.Categories.FirstOrDefault(x => x.CategoryId == newProduct.CategoryId);
-            if (existingCategory != null)
+            if (existingCategory == null)
             {
                 newProduct.CategoryId = 1;
             }
             NorthwindContext.Products.Add(newProduct);
             NorthwindContext.SaveChanges();
             return View();
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var existingProduct = NorthwindContext.Products.FirstOrDefault(x => x.ProductId == id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            return View(existingProduct);
+        }
+        [HttpPost]
+        public IActionResult Update(Product updatedProduct)
+        {
+            var existingProduct = NorthwindContext.Products.FirstOrDefault(x => x.ProductId == updatedProduct.ProductId);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            existingProduct.ProductName = updatedProduct.ProductName;
+            existingProduct.Cost = updatedProduct.Cost;
+            existingProduct.Stock = updatedProduct.Stock;
+            existingProduct.CategoryId = updatedProduct.CategoryId;
+            NorthwindContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
